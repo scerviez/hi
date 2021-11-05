e3aa871a899a8ae6742e0133d2c2f63a893e1afb
Exex <81870902+exexme@users.noreply.github.com>
2021-11-05 15:37:56 +0700

    Create gabut.js

---
+++ gabut.js
@@ -0,0 +1,10 @@
+function tebakumur(text) {
+   var tebak = text
+    if (tebak == '100'){
+        console.log('benar')
+    }else{
+        console.log('salah')
+}
+function percobaan() {
+   tebakumur('100') // benar
+}
